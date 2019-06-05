6469ffca9877c617379f9680cec693a09db7e4f5
Olkris2666 <olkris2666@gmail.com>
2019-06-05 03:57:53 +0200

    Added the abillity to save new tags

--- taglogic.js
+++ taglogic.js
@@ -1,9 +1,42 @@
 if (typeof(Storage) !== "undefined") {
-  // browser has Web Storage support > code
+// executes page code only if web storage features are supported
 
+  function saveTags(currentTags) {
+    localStorage.setItem("tags", currentTags.join(","));
+  }
+  // stores given parameter to localStorage in string format
 
+  function loadTags() {
+    if (localStorage.getItem("tags") !== null) {
+      var tagString = localStorage.getItem("tags");
+      var tagArray = tagString.split(",");
+    }
+    // converts the tag string into an array
+    else {
+      var tagArray = [];
+    }
+    // creates an empty array
+    return tagArray;
+  }
+  // returns saved tags as an array
+
+  function addTag(userInput) {
+    currentTags = loadTags();
+    currentTags.push(userInput);
+    saveTags(currentTags);
+  }
+  // saves the given parameter at the end of the localStorage tag string
+
+  function deleteTag(tagNumber) {
+
+    saveTags();
+  }
+
+  function showTags() {
+
+  }
 
 } else {
-  //no Web Storage support > error message
-  document.write("Web Storage is unsuported on this browser. Please try updating it to its latest version, or using another one.")
+  document.write("Web Storage is unsuported on this web browser. Please try updating it to its latest version, or using another one.");
 }
+// gives an eror message if web storage features are not supported
